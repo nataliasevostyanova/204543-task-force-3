f12580d4ad997f69238d1daaabcd3315ba859bcb
Natalia Sevostyanova <sevostyanova@gmail.com>
2021-11-09 17:31:24 +0300

    Тест действие-статус

--- testing/test_m1t2_actualStatus.php
+++ testing/test_m1t2_actualStatus.php
@@ -1,13 +1,10 @@
 <?php
 /**
- * File
- * for testing if TaskStatusAction works right way
+ *
+ * testing class TaskStatusAction method getActualStatus
  */
 
 require_once '../classes/TaskStatusAction.php';
-
-
-//настройки assert()
 
 assert_options(ASSERT_ACTIVE, 1);
 assert_options(ASSERT_WARNING, 1);
@@ -18,15 +15,12 @@ assert_options(ASSERT_CALLBACK, function () {
 });
 
 
-$strategy = new TaskStatusAction(5, 2, 'client', 'new');
-/**
- * проверка работы метода getActualStatus()
- */
+$strategy = new TaskStatusAction(5, 2);
 
 assert($strategy->getActualStatus(TaskStatusAction::ACTION_CANCEL) == TaskStatusAction::STATUS_UNDO, 'Problem with cancel action: expected status "UNDO"');
 assert($strategy->getActualStatus(TaskStatusAction::ACTION_RESPOND) == TaskStatusAction::STATUS_WORKING, 'Problem with respond action: expected status "WORKING"');
 assert($strategy->getActualStatus(TaskStatusAction::ACTION_FINISH) == TaskStatusAction::STATUS_FINISH, 'Problem with finish action: expected status "FINISH"');
 assert($strategy->getActualStatus(TaskStatusAction::ACTION_REFUSE) == TaskStatusAction::STATUS_REFUSAL, 'Problem with refuse action; expected status "REFUSAL"');
 
-assert(false, 'test TaskStatusAction::getActualStatus() complete THE END');
+assert(false, 'test TaskStatusAction::getActualStatus() is complete THE END');
 
