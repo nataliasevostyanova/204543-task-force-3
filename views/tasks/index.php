4e8eb405da561909819aeefbdd9a6a30caebc418
Natalia Sevostyanova <sevostyanova@gmail.com>
2022-02-07 15:23:01 +0300

    state 07.02.22 15.22msk

--- views/tasks/index.php
+++ views/tasks/index.php
@@ -51,9 +51,9 @@
     <!-- блок выбора задач start -->
     <div class="right-column">
        <div class="right-card black">
-           <div class="search-form">
-                <form>
-                    <h4 class="head-card">Категории</h4>
+          <div class="search-form">
+             <form>
+                <h4 class="head-card">Категории</h4>
                     <div class="form-group">
                         <div>
                             <input type="checkbox" id="сourier-services" checked>
@@ -63,24 +63,24 @@
                             <input id="translations" type="checkbox">
                             <label class="control-label" for="translations">Переводы</label>
                         </div>
-                    </div>
-                    <h4 class="head-card">Дополнительно</h4>
-                    <div class="form-group">
-                        <input id="without-performer" type="checkbox" checked>
-                        <label class="control-label" for="without-performer">Без исполнителя</label>
-                    </div>
-                    <h4 class="head-card">Период</h4>
-                    <div class="form-group">
+                </div>
+                <h4 class="head-card">Дополнительно</h4>
+                <div class="form-group">
+                    <input id="without-performer" type="checkbox" checked>
+                     <label class="control-label" for="without-performer">Без исполнителя</label>
+                </div>
+                <h4 class="head-card">Период</h4>
+                <div class="form-group">
                         <label for="period-value"></label>
                         <select id="period-value">
                             <option>1 час</option>
                             <option>12 часов</option>
                             <option>24 часа</option>
                         </select>
-                    </div>
-                    <input type="button" class="button button--blue" value="Искать">
-                </form>
-           </div>
+                </div>
+                <input type="button" class="button button--blue" value="Искать">
+            </form>
+          </div>
        </div>
     </div>
     <!-- блок выбора задач end -->
